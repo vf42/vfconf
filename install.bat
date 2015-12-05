ec1c41a4fdc14c11d753c64f57f6e70d619ecda0
Vadim Fedorov <vadim.fedorov@gmail.com>
2015-12-05 13:22:09 +0200

    Added vimfiles creation on windows.

--- install.bat
+++ install.bat
@@ -8,6 +8,9 @@ for /d %%i in (*) do (
         mklink /d "%UserProfile%\%%~i" "%%~fi"
     )
 )
+rem On Windows, need to use Vimfiles for .vim
+rmdir /s /q "%UserProfile%\vimfiles"
+mklink /d "%UserProfile%\vimfiles" "%UserProfile%\.vim"
 
 for %%i in (*) do (
     IF NOT "%%~i" == "install.bat" IF NOT "%%~i" == "install.sh" (
